89e34f908320c0d06e6f4c61bc524c83976897e9
gunjanmistry08 <gunjanmistry008@gmail.com>
2023-05-11 23:06:04 +0530

    added search and mongo connection

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ require('dotenv').config()
 const express = require('express');
 const expressLayout = require('express-ejs-layouts')
 
+const connectDB = require('./server/config/db')
+
 const app = express();
 const PORT = 5000 || process.env.PORT;
+
+connectDB();
 
 app.use(express.static('public'))
 
 app.use(expressLayout)
+
 app.set('layout','./layout/main')
 app.set('view engine','ejs')
 
