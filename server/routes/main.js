b65d3a14dfa80c8bf3fe6ba36de743784fdc501a
gunjanmistry08 <gunjanmistry008@gmail.com>
2023-05-13 01:23:55 +0530

    added post backend connection

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,13 +1,25 @@
 const express = require('express')
+const Post = require('../models/Post')
 
 const router = express.Router()
 
-router.get('',(req,res) => {
+router.get('',async (req,res) => {
     var local = {
         title: "BlogSphere",
         desc: "Blogsphere best site"
     }
-    res.render('index',{local})
+    
+    try {
+        let perPage = 10
+        let page = req.query.page || 1
+        const posts = await Post.aggregate([{$sort:{createdAt: -1}}]).skip(perPage*page -perPage).limit(perPage).exec()
+        const postCount = await Post.count()
+        const nextPage = parseInt(page)+1
+        const hasNextPage = nextPage <= Math.ceil(postCount/perPage)
+        res.render('index',{local, posts,currentPage:page,nxtPage:hasNextPage? nextPage : null})
+    } catch (error) {
+        console.log(error);
+    }
 })
 
 router.get('/about',(req,res) => {
